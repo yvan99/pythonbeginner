1c86c9ae29c1b96005ddd3b4b22447664d2d7385
yvan99 <ishimweyvan90@gmail.com>
2023-03-26 12:11:56 +0200

    functions

---
+++ functions.py
@@ -0,0 +1,6 @@
+def sumCalculator(firstNumber,secondNumber):
+    sum = firstNumber + secondNumber
+    return sum
+
+print(sumCalculator(10,20)) # positional argument
+print(sumCalculator( firstNumber= 10, secondNumber= 20)) #keyword argument
